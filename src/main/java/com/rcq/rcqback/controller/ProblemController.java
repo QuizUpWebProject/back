7dd323bce495afa5960b30f0e4c84a58ba188931
good9804 <ggoggoni98@gmail.com>
2023-09-30 00:57:36 +0900

    [Controller]기준에 따른 문제 목록 조회 기능 수정, [Enum]문제리스트 조회 기준 Enum 생성, [DTO]문제 목록 조회 dto 생성

--- src/main/java/com/rcq/rcqback/controller/ProblemController.java
+++ src/main/java/com/rcq/rcqback/controller/ProblemController.java
@@ -1,13 +1,17 @@
 package com.rcq.rcqback.controller;
 
 
+import com.rcq.rcqback.dto.problem.checkProblemListDto;
 import com.rcq.rcqback.dto.problem.getProblemListDto;
+import com.rcq.rcqback.dto.problem.makeProblemDto;
 import com.rcq.rcqback.service.problem.ProblemService;
 import com.rcq.rcqback.util.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -23,14 +27,27 @@ public class ProblemController {
     }
 
     @GetMapping("/api/getlist")
-    public ResponseEntity<ApiResponse> getProblemList(@RequestParam("nickname") String category){
+    public ResponseEntity<ApiResponse> getProblemList(@RequestBody checkProblemListDto checkProblemListDto){
         ApiResponse apiResponse=new ApiResponse();
-        List<getProblemListDto> problemListDtos= problemService.getProblemList(category);
+        List<getProblemListDto> problemListDtos=problemService.getProblemList(checkProblemListDto);
         if(problemListDtos.size()>0){
             apiResponse.setResult(problemListDtos);
             apiResponse.setSuccessResponse();
         }else{
             apiResponse.setNOTFOUNDResponse("해당 카테고리에 문제목록이 존재하지않습니다.");
+        }
+        return new ResponseEntity<>(apiResponse, apiResponse.getHttpStatus());
+    }
+
+    @PostMapping("/api/makeproblem")
+    public ResponseEntity<ApiResponse> makeProblem(makeProblemDto makeproblemdto){
+        ApiResponse apiResponse=new ApiResponse();
+        try{
+        problemService.saveProblem(makeproblemdto);
+        apiResponse.setSuccessResponse();
+        }catch (Exception e){
+            String message="[문제 생성중 오류]";
+            apiResponse.setINTERNAL_SERVER_ERRORResponse(message+e.getMessage());
         }
         return new ResponseEntity<>(apiResponse, apiResponse.getHttpStatus());
     }
--- src/main/java/com/rcq/rcqback/service/problem/ProblemService.java
+++ src/main/java/com/rcq/rcqback/service/problem/ProblemService.java
@@ -2,6 +2,7 @@ package com.rcq.rcqback.service.problem;
 
 
 import com.rcq.rcqback.dto.auth.signUpUserDto;
+import com.rcq.rcqback.dto.problem.checkProblemListDto;
 import com.rcq.rcqback.dto.problem.getProblemListDto;
 import com.rcq.rcqback.dto.problem.makeProblemDto;
 import com.rcq.rcqback.entity.User;
@@ -9,13 +10,13 @@ import com.rcq.rcqback.entity.problem.Problem;
 import com.rcq.rcqback.entity.problem.ProblemList;
 import com.rcq.rcqback.repository.problem.ProblemListRepository;
 import com.rcq.rcqback.repository.problem.ProblemRepository;
+import com.rcq.rcqback.util.ProblemListStandardEnum;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
@@ -32,18 +33,37 @@ public class ProblemService {
         problem.setQuestion(makeProblemDto.getQuestion());
         problem.setAnswer(makeProblemDto.getAnswer());
         problem.setProblemList(problemListRepository.findByid(makeProblemDto.getProblemlistid()));
-
-        return problemRepository.save(problem);
+            return problemRepository.save(problem);
     }
 
-    public List<getProblemListDto> getProblemList(String category){
+    @Transactional
+    public List<getProblemListDto> getProblemList(checkProblemListDto checkProblemListDto){
         List<getProblemListDto> dtoList = new ArrayList<>();
-        List<ProblemList> problemLists = problemListRepository.findAllByCategory(category);
+        List<ProblemList> problemLists = problemListRepository.findAllByCategory(checkProblemListDto.getCategory());
+        problemLists=sortProblemList(problemLists,checkProblemListDto.getStandardEnum());
         for(ProblemList problemList: problemLists){
             getProblemListDto dto=modelMapper.map(problemList,getProblemListDto.class);
             dtoList.add(dto);
         }
         return dtoList;
+    }
+
+    public List<ProblemList> sortProblemList(List<ProblemList> problemLists, ProblemListStandardEnum standardEnum){
+        Comparator<ProblemList> comparator = null;
+        if(standardEnum==ProblemListStandardEnum.LATEST){
+            comparator=Comparator.comparing(ProblemList::getId).reversed();
+        }
+        if(standardEnum==ProblemListStandardEnum.OLDEST){
+            comparator=Comparator.comparing(ProblemList::getId);
+        }
+        if(standardEnum==ProblemListStandardEnum.RECOMMEND){
+            comparator=Comparator.comparing(ProblemList::getRecommendcount).reversed();
+        }
+        if(standardEnum==ProblemListStandardEnum.VIEW){
+            comparator=Comparator.comparing(ProblemList::getViewcount).reversed();
+        }
+        problemLists.sort(comparator);
+        return problemLists;
     }
 
 
